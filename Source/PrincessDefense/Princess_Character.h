15926499d6061d342ac9a8e8e011260c6fe947fc
sakaguchi5 <56575533+sakaguchi5@users.noreply.github.com>
2021-05-12 11:47:39 +0900

    a

---
+++ Source/PrincessDefense/Princess_Character.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character_Master.h"
+#include "Princess_Character.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRINCESSDEFENSE_API APrincess_Character : public ACharacter_Master
+{
+	GENERATED_BODY()
+	
+};
